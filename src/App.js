99ea5c37b376d8fb86b080c1312aea88b433718a
zabihullah <zabihullahyaqubi58@gmail.com>
2024-01-11 16:58:42 +0430

    starting project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+   <ExpenseItem ></ExpenseItem >
+    <p>this is also visible</p>
+    </div>
+  );
+} 
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+
+import  ex from './ExpenseItem.css'
+function ExpenseItem() {
+    return (
+      <div  className="expense-Item">
+     <div >Date</div>
+     <h2> title</h2>
+     <div> 20.9$</div>
+      </div>
+    );
+    
+  }
+  
+  export default ExpenseItem;
+  
