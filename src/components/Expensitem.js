b0387e09aaa516eed5c590776c2b94a743a7e6f8
zabihullah <zabihullahyaqubi58@gmail.com>
2024-01-25 21:07:09 +0430

    working on expense component

--- src/components/Expensitem.js
+++ src/components/Expensitem.js
@@ -1,10 +1,15 @@
 import "./Expensitem.css";
-function ExpensItem() {
+function ExpensItem(props) {
+//   const expenseDate = new Date(2021, 2, 21);
+//   const expenseTitle = "Car insurance";
+//   const expensPrice = 234.89;
   return (
     <div className="expens-item">
-      <div> 20 march 2021 </div>
-      <h2 className="expens-descrption"> Car Insurance </h2>
-      <div className="expens-price"> $232.32</div>
+      <div> {props.date.toISOString()} </div>
+      <div className="expens-descrption">
+        <h2 className="h2"> {props.title} </h2>
+        <div className="expens-price"> ${props.amount}</div>
+      </div>
     </div>
   );
 }
